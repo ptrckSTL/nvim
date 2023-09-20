4e9263a36a797b278fa7c3cc720c72b9dbdff6a5
Patrick Kellison <patrick@Patricks-MacBook-Pro.local>
2023-09-20 11:44:59 -0700

    init

---
+++ lua/plugins/gp.lua
@@ -0,0 +1,11 @@
+return {
+  "Robotx/gp.nvim",
+  config = function()
+    require("gp").setup()
+
+    -- or setup with your own config (see Install > Configuration in Readme)
+    -- require("gp").setup(conf)
+
+    -- shortcuts might be setup here (see Usage > Shortcuts in Readme)
+  end,
+}
